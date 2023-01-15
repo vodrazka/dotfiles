cc2a894fa744affbc188c740d1e6e1d211508855
Thomas Vodrazka <9295830+vodrazka@users.noreply.github.com>
2023-01-15 08:43:13 +0100

    added folding

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -74,6 +74,7 @@ return packer.startup(function(use)
     use 'https://git.sr.ht/~whynothugo/lsp_lines.nvim'
     use 'mrjones2014/smart-splits.nvim'
     use 'ggandor/leap.nvim'
+    use { 'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async' }
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
