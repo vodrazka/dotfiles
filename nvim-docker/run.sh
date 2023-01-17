bab6c3dd7a3756def9eee6c054aae0bc3251372e
Thomas Vodrazka <9295830+vodrazka@users.noreply.github.com>
2023-01-17 09:35:02 +0100

    removed docker nvim

--- nvim-docker/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm -v nvim-vol:/root/.local/share/nvim/ -v /Users/thomas/.config/nvim/init.lua:/root/.config/nvim/init.lua:ro -v /Users/thomas/.config/nvim/lua/:/root/.config/nvim/lua/:ro nvim $1
--- nvim-docker/shell.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm --entrypoint /bin/sh nvim
