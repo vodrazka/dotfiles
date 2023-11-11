8f11cd29144b61e5fbffcaade0f509be2c91a7ea
Thomas Vodrazka <9295830+vodrazka@users.noreply.github.com>
2023-11-11 12:06:57 +0100

    bufferline

--- nvim/lua/vodrazka/maps.lua
+++ nvim/lua/vodrazka/maps.lua
@@ -1,4 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>.', ":set list!<CR>", { desc = "Toggle whitespaces"})
 vim.keymap.set('n', '<leader>,', ":set hls!<CR>", { desc = "Toggle highlight search"})
+vim.keymap.set('n', '<leader>j', ":bprevious<CR>", { desc = "Buffer previous"})
+vim.keymap.set('n', '<leader>k', ":bnext<CR>", { desc = "Buffer next"})
+vim.keymap.set('n', '<leader>x', ":bdelete<CR>", { desc = "Buffer delete"})
+vim.keymap.set('n', '<leader>t', ":tabnew<CR>", { desc = "Tab new"})
 vim.cmd.colorscheme "koehler"
