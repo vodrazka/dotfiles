0cc5ce7a29593f70a1b4007923fbc09e9146cd6b
Thomas Vodrazka <9295830+vodrazka@users.noreply.github.com>
2023-11-11 12:14:16 +0100

    better-buf-del

--- nvim/lua/vodrazka/maps.lua
+++ nvim/lua/vodrazka/maps.lua
@@ -3,6 +3,6 @@ vim.keymap.set('n', '<leader>.', ":set list!<CR>", { desc = "Toggle whitespaces"
 vim.keymap.set('n', '<leader>,', ":set hls!<CR>", { desc = "Toggle highlight search"})
 vim.keymap.set('n', '<leader>j', ":bprevious<CR>", { desc = "Buffer previous"})
 vim.keymap.set('n', '<leader>k', ":bnext<CR>", { desc = "Buffer next"})
-vim.keymap.set('n', '<leader>x', ":bdelete<CR>", { desc = "Buffer delete"})
+vim.keymap.set('n', '<leader>x', ":bp<bar>sp<bar>bn<bar>bd<CR>", { desc = "Buffer delete"}) --delete but do not exit, strange that vim closes the windows by default...
 vim.keymap.set('n', '<leader>t', ":tabnew<CR>", { desc = "Tab new"})
 vim.cmd.colorscheme "koehler"
