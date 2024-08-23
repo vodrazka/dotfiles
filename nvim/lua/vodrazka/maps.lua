e35a68998082c20e9a40d0949ea7c92f1f1ae48c
Thomas Vodrazka <9295830+vodrazka@users.noreply.github.com>
2024-08-23 16:09:33 +0200

    change tab to new buf

--- nvim/lua/vodrazka/maps.lua
+++ nvim/lua/vodrazka/maps.lua
@@ -4,7 +4,7 @@ vim.keymap.set('n', '<leader>,', ":set hls!<CR>", { desc = "Toggle highlight sea
 vim.keymap.set('n', '<leader>k', ":bprevious<CR>", { desc = "Buffer previous"})
 vim.keymap.set('n', '<leader>j', ":bnext<CR>", { desc = "Buffer next"})
 vim.keymap.set('n', '<leader>x', ":bp<bar>sp<bar>bn<bar>bd!<CR>", { desc = "Buffer delete"}) --delete but do not exit, strange that vim closes the windows by default...
-vim.keymap.set('n', '<leader>t', ":tabnew<CR>", { desc = "Tab new"})
+vim.keymap.set('n', '<leader>t', ":enew<CR>", { desc = "new buffer"})
 vim.keymap.set('n', '<leader>l', ":normal! ggVG=<C-O><CR>", { desc = "Format file"})
 vim.keymap.set('n', '<leader>w', ":cclose<CR>", { desc = "Close quickfix"})
 vim.cmd.colorscheme "koehler"
