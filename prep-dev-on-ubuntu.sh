e4fbf509d24b73212a656dbf5c782f515d2aad86
Thomas Vodrazka <9295830+vodrazka@users.noreply.github.com>
2025-04-05 23:21:07 +0000

    bashrc minors

--- prep-dev-on-ubuntu.sh
+++ prep-dev-on-ubuntu.sh
@@ -39,5 +39,7 @@ wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.2.linux-amd64.tar.gz
 rm go1.24.2.linux-amd64.tar.gz
 cat <<\EOF >> /root/.bashrc
+alias vim=nvim
+export EDITOR=vim
 export PATH=$PATH:/usr/local/go/bin
 EOF
